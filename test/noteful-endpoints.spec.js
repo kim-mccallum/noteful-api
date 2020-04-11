d04aa54d7e19fc538fa37565d062d7d067b30951
Kim McCallum <kchaase@gmail.com>
2020-04-11 16:40:59 -0600

    set up deployment

--- test/noteful-endpoints.spec.js
+++ test/noteful-endpoints.spec.js
@@ -8,7 +8,7 @@ describe.only('Noteful Endpoints', function() {
   
       db = knex({
         client: 'pg',
-        connection: process.env.TEST_DB_URL,
+        connection: process.env.TEST_DATABASE_URL,
       })
       app.set('db', db)
   
